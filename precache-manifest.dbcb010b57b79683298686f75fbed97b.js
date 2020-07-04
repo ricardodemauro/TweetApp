2daa43c065e4ff3c291b51c090e61d1c2d665ad7
Ricardo Mauro <ricardo.demauro@outlook.com>
2020-07-04 11:02:42 -0300

    Updates

---
+++ precache-manifest.dbcb010b57b79683298686f75fbed97b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c5f527fd7a8abcb09e36f29130762d",
+    "url": "/TweetApp/index.html"
+  },
+  {
+    "revision": "5d4be4de2923ebdff407",
+    "url": "/TweetApp/static/css/main.af14a1ab.chunk.css"
+  },
+  {
+    "revision": "2a8cae81b8aaa4c58089",
+    "url": "/TweetApp/static/js/2.94f26320.chunk.js"
+  },
+  {
+    "revision": "15c33a81f958097d85fb38e5c966519d",
+    "url": "/TweetApp/static/js/2.94f26320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d4be4de2923ebdff407",
+    "url": "/TweetApp/static/js/main.748e090a.chunk.js"
+  },
+  {
+    "revision": "e1bcdb50539a74fffff4",
+    "url": "/TweetApp/static/js/runtime-main.33550920.js"
+  }
+]);
