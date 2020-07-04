403e38e0a75882044600264e6fa728ebc5c95595
Ricardo Mauro <ricardo.demauro@outlook.com>
2020-07-04 10:17:46 -0300

    adding readme and fixing github link at footer

---
+++ Readme.md
@@ -0,0 +1,26 @@
+## Mini Tweet App
+
+Created during sessions at twitch.tv/rmaurodev
+
+Published at: https://tweet.rmaurolabs.dev
+
+## Architecture
+
+Frontend SPA - ReactJS + Bootstrap
+
+Backend Serverless - Azure Functions + C#
+
+Database - Azure Table Storage (NoSQL)
+
+Config - Azure Key Vault
+
+## Contributors
+
+´´´
+const contributors = [{
+	name: 'Ricardo',
+	alias: 'rmaurodev'
+	twitch: 'twitch.tv/rmaurodev,
+	blog: 'https://rmauro.dev'
+}]
+´´´
--- TweetApp.SPA/src/components/footer.js
+++ TweetApp.SPA/src/components/footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="mastfoot mt-auto">
             <div className="inner">
-                <p>POC made over twitch streams on <a target="_blank" rel="noopener noreferrer" href="https://rmauro.dev/twitch">RMauro's Twitch</a>. Source code on <a href="https://github.com/ricardomauro">@Github</a>.</p>
+                <p>POC made over twitch streams on <a target="_blank" rel="noopener noreferrer" href="https://rmauro.dev/twitch">RMauro's Twitch</a>. Source code on <a href="https://github.com/ricardodemauro/TweetApp">Github</a>.</p>
             </div>
         </footer>
     );
